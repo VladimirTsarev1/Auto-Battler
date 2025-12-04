d5e8cd94c66ad3ebdda76319335cf7556b12a384
Vladimir Tsarev <vladimir.csarev@gmail.com>
2025-12-04 23:27:39 +0500

    Add TMP, Main Menu (WIP)

---
+++ Assets/_Project/Scripts/MainMenu/MainMenuPanelView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.MainMenu
+{
+    public class MainMenuPanelView : MonoBehaviour
+    {
+        public event Action PlayButtonClicked;
+
+        [SerializeField] private Button playButton;
+
+        public void Init()
+        {
+            playButton.onClick.AddListener(HandlePlayButtonClicked);
+        }
+
+        private void HandlePlayButtonClicked()
+        {
+            PlayButtonClicked?.Invoke();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Root/MainMenuInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace _Project.Scripts.Root
+{
+    public class MainMenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
